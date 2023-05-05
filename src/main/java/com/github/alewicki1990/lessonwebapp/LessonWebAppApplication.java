d03a2936dcae9a7e59c3bb24bf8fa9fab344ba2a
Arkadiusz Lewicki <alewicki1990@gmail.com>
2023-05-05 20:05:52 +0200

    New project

---
+++ src/main/java/com/github/alewicki1990/lessonwebapp/LessonWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.alewicki1990.lessonwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alewicki1990/lessonwebapp/LessonWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alewicki1990.lessonwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
